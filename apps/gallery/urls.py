5aba994d8797128a9afec7136eed1925b48269ae
Praise Idowu <ifeoluwapraise02@gmail.com>
2024-03-21 12:40:07 +0100

    Urls, views, and templates for CategoryDetail and PhotoListView, added pagination

--- apps/gallery/urls.py
+++ apps/gallery/urls.py
@@ -5,4 +5,6 @@ app_name = 'gallery'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('category/<slug:slug>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('photos/', views.PhotoListView.as_view(), name="photo_list"),
 ]
--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 # from apps.accounts.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Photo, Video, Tag, Category
 
 class HomeView(View):
@@ -23,10 +23,36 @@ class HomeView(View):
         return render(request, 'gallery/home.html', context)
 
 class CategoryDetailView(View):
-    pass
+    def get(self, request, *args, **kwargs):
+        category = get_object_or_404(Category, slug=kwargs["slug"])
+        photos = category.photos.all().order_by("-created_at") # retrieve all photos associated with d categ
+        context = {
+            "category": category,
+            "photos": photos
+        }
+        return render(request, "gallery/category/category_detail.html", context)
+        
 
 class PhotoListView(View):
-    pass
+    def get(self, request):
+        photos = Photo.objects.all().order_by("-created_at")
+        paginator = Paginator(photos, 16)  # num of photos per page
+        page = request.GET.get("page")
+        
+        try:
+            photo_list = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page
+            photo_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range, deliver last page of results
+            photo_list = paginator.page(paginator.num_pages)
+        
+        context = {
+            "photos": photos,
+            "photo_list": photo_list
+        }    
+        return render(request, "gallery/photos/photo_list.html", context)
 
 class PhotoDetailView(View):
     pass
